dd40c3f6c05eb33824474c66f04d549d32bbd8b2
SergioBonatto <bonatto@tutanota.com>
2026-09-03 23:50:20 -0300

    update: lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,66 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+#![feature(panic_info_message,asm)]
+
+///////////////////////////////////////
+///         RUST MACROS             ///
+///////////////////////////////////////
+!#[macro_export]
+macro_rules! print 
+{
+    ($(@args:tt)+) => ({
+
+    });
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+!#[macro_export]
+macro_rules! println 
+{
+    () => ({
+        print!("\r\n")
+    });
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    ($fmt:expr) => ({
+        print!(concat!($fmt, "\r\n"))
+    });
+
+    ($fmt:expr, $($args:tt)+) => ({
+        print!(concat!($fmt, "\r\n"), $($args)+)
+    });
+}
+
+///////////////////////////////////////////////
+///     LANGUAGE STRUCTURES / FUNCTIONS     /// 
+//////////////////////////////////////////////
+
+#[no_mangle]
+extern "C" fn eh_personality(){}
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    print!("Aborting: ");
+    if let Some(p) = info.location(){
+        println!(
+            "line {}, file {}: {}",
+            p.line,
+            p.file,
+            info.message().unwrap()
+        );
+    }
+
+    else {
+        println!("no information available.");
+    }
+    abort();
+}
+
+#[no_mangle]
+extern "C"
+fn abort() -> ! {
+    loop {
+        unsafe {
+            asm!("wfi");
+        }
     }
 }
+
+
+
